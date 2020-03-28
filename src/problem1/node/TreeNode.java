49a4bf0196f33c796e173b2b1cc8a7fa6767b0c7
Adamaya <adamaya.sharma_iot18@gla.ac.in>
2020-03-28 15:37:34 +0530

    getter setter created

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,13 +7,35 @@
 package problem1.node;
 
 public class TreeNode {
-    int data;
-    TreeNode left, right;
+    public int data;
+    public TreeNode left, right;
 
-    public TreeNode(int item)
-    {
+    public TreeNode(int item) {
         data = item;
         left = right = null;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
